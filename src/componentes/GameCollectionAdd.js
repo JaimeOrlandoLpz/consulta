540184ded3452e7b0f6615873070451f727eba68
Jaime Orlando Lopez Ramos <30807749+JaimeOrlandoLpz@users.noreply.github.com>
2021-10-05 21:50:20 -0500

    Collection

--- src/componentes/GameCollectionAdd.js
+++ src/componentes/GameCollectionAdd.js
@@ -1,36 +1,53 @@
-import React, { useState } from "react";
-import { getVideojuegosByID } from "./GetVideoJuegosByID";
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
 
+export const GameCollectionAdd = ({dispatch}) => {
+    //En la variable inputValue siempre vamos a tener el último valor que el usuario escribió en
+    // el input text.
+    const [inputValue, setInputValue] = useState('Indica el Título');
 
-export function GameCollectionAdd(){
-
-    const [titleID, setTitleID] = useState('Indique el ID del título de su elección');
-    const [juego, setJuego] = useState({
-        id: 300000,
-        name: "Halleloo"
-    });
+    //Esta función se invoca cada vez que se detecta el evento onChange en el input text.
     const handleInputChange = (e) => {
-        setTitleID(e.target.value);
+        setInputValue(e.target.value);
     }
 
+    //Esta función se invoca cada vez que se presiona la tecla enter en el input text.
     const handleSubmit = (e) => {
         e.preventDefault();
         console.log('Se hizo submit...');
-        getVideojuegosByID(titleID).then((resultado) =>{
-            setJuego(resultado);
-        });
-        console.log('----------');
-        console.log(juego);
-            // Limpiamos input value
-         setTitleID('');}
 
-    return(
-        <div>
-            <form onSubmit={handleSubmit}>
-                <div className="input-group input-group-sm mb-3">
-                <input type="text" onChange={ handleInputChange } value={titleID} className="form-control"/></div>
-            </form>
-            <p>Juego: {juego.nombre}</p>
-        </div>
-    );
+        if (0 < Number(inputValue)) {
+            dispatch({type:'add', payload:inputValue});
+            setInputValue('');
+        }
+    }
+
+    return (
+        /*
+        Quitamos el fragment <></> y lo cambiamos por un form para poder invocar la función
+        de handleSubmit.
+        */
+        <form onSubmit={handleSubmit}>
+            {/*
+            Creamos un input text para que el usuario escriba el videojuego que quiere
+            agregar a la lista, y hacemos un binding a la variable inputValue en la que vamos a guardar
+            lo que escriba el usuario en este input text.
+            Cada vez que se escriba algo en el input text se manda llamar la función handleInputCange la
+            cual va actualizar el estado de la variable inputValue.
+            */}
+            <div className="input-group input-group-sm mb-3">
+                <input 
+                    type="text"
+                    value={inputValue}
+                    onChange={ handleInputChange /*TODO make not avaialbe to submit*/ }
+                    className="form-control"
+                />
+                <button className="btn btn-primary" type="submit">Agregar</button>
+            </div>
+        </form>
+)
+}
+
+GameCollectionAdd.propTypes = {
+    setTitulos: PropTypes.func.isRequired
 }
--- src/componentes/GameCollectionApp.js
+++ src/componentes/GameCollectionApp.js
@@ -1,11 +1,29 @@
-import React from "react";
-import {GameCollectionAdd} from './GameCollectionAdd';
+import React, { useReducer } from 'react';
+import { GameCollectionAdd } from './GameCollectionAdd';
+import { GameCollection } from './GameCollection';
+import { collectionReducer, init } from './hooks/collectionReducer';
 
-export function GameCollectionApp(){
-    return(
-        <div>
-            <GameCollectionAdd></GameCollectionAdd>
-        </div>
-    );
+
+export const GameCollectionApp = () => {
+
+    const [titulos, dispatch] = useReducer(collectionReducer, [], init);
+
+
+    return ( 
+        <>
+            <div className = "jumbotron jumbotron-fluid" >
+                <div className = "container" >
+                <h1 className = "display-4" > Gamebook </h1> 
+                <p className = "lead" > ¡Bienvenido a la página donde podrás consultar información de videojuegos! </p> 
+                </div> 
+            </div> 
+
+            <GameCollectionAdd dispatch = {dispatch} />
+
+
+            {/*Creamos lista*/}
+            <GameCollection titulos={titulos} dispatch={dispatch}/>
+            
+        </>
+    )
 }
-
