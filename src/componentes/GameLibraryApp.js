19eda10c8121ad84afec487b44bd7322ed2e15ee
Jaime Orlando Lopez Ramos <30807749+JaimeOrlandoLpz@users.noreply.github.com>
2021-11-01 11:40:14 -0600

    Context Complete

--- src/componentes/GameLibraryApp.js
+++ src/componentes/GameLibraryApp.js
@@ -6,7 +6,9 @@ import { UserContext } from './hooks/context/UserContext';
 export const GameLibraryApp = () => {
     //Utilizamos useState para mantener un usuario y compartirlo con los demás componentes por medio del Context. 
     // El estado inicial es un objeto vacío. 
-    const [user, setUser] = useState({}); 
+    const [user, setUser] = useState({
+        username: "No User"
+    }); 
     return (
         <UserContext.Provider value = {{user, setUser}}>
             <MainAppRouter/>
--- src/componentes/comunes/Navbar.js
+++ src/componentes/comunes/Navbar.js
@@ -1,7 +1,16 @@
-import React from 'react';
+import React, {useState, useContext} from 'react';
 //Con Link y NavLinkvamos a poder navegar entre las diferentes páginas utilizando el Router de React. 
 import { Link, NavLink } from 'react-router-dom';
+import { UserContext } from '../hooks/context/UserContext';
+
 export const Navbar = () => {
+
+
+    const { user, setUser } = useContext(UserContext);
+    console.log("user: " + user.username);
+
+
+
     return (
         <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
             <Link className="navbar-brand" to="/">Librería</Link>
@@ -14,11 +23,19 @@ export const Navbar = () => {
                 </div>
             </div>
             <div className="navbar-collapse collapse w-100 order-3 dual-collapse2">
+ 
                 <ul className="navbar-nav ml-auto">
                     <NavLink activeClassName="active" className="nav-item nav-link" exact to="/welcome">
                         Salir
                     </NavLink>
                 </ul>
+
+                <div className="navbar-nav ml-auto">
+                    <p className="navbar-text"> User: { user.username }</p>
+                </div>
             </div>
+
+
+
         </nav>);
 }
