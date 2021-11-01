d24d8e8913742ff4bfef61c36b9cf3149019d837
Jaime Orlando Lopez Ramos <30807749+JaimeOrlandoLpz@users.noreply.github.com>
2021-10-31 21:18:41 -0600

    Login Hardcodedd

--- src/componentes/WelcomePage.js
+++ src/componentes/WelcomePage.js
@@ -1,15 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 
-export const WelcomePage = ({history}) => {
-    const doStart = () =>{
-        history.replace('/videojuegos');
+export const WelcomePage = ({ history }) => {
+
+    const hardcodedUser = {
+        username: 'Jorge',
+        password: 'Miramontes'
+
     }
-    return(
+
+    const [userData, setUserData] = useState({
+        username: '',
+        password: ''
+    });
+
+    const usernameHandler =(event)=>{
+        
+        setUserData((prev)=>{
+            return {
+                ...prev,
+                username: event.target.value
+            }
+        });
+    }
+    const passwordHandler =(event)=>{
+        setUserData((prev)=>{
+            return {
+                ...prev,
+                password: event.target.value
+            }
+        });
+    }
+    const doStart = (event) => {
+        event.preventDefault();
+        if(hardcodedUser.username === userData.username && hardcodedUser.password === userData.password){
+            history.replace('/videojuegos');
+        }
+        else{
+            alert("Wrong Credentials");
+        }
+        
+    }
+    return (
+        <form style={{textAlign: 'center'}} onSubmit={doStart}>
+            <div className="form-group">
+                <label>Username</label>
+                <input className="form-control" type="text" name="username" onChange = {usernameHandler} value = {userData.username}></input>
+            </div>
+
         <div>
-            <h1>Welcome to the Videogame Library App</h1>
-            <br/>
-            <button className="btn btn-primary"onClick={ doStart }>Acceder</button>
+            <label>Password</label>
+            <input className="form-control" type="password" name="password" onChange = {passwordHandler} value={userData.password}></input>
         </div>
+
+
+            <button type="submit" className="btn btn-primary" style={{marginTop: '1.5rem'}}>Ingresar</button>
+        </form>
     );
 }
--- src/routers/SecondaryRouter.js
+++ src/routers/SecondaryRouter.js
@@ -3,6 +3,7 @@ import { Navbar } from '../componentes/comunes/Navbar';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import { GameCollectionApp } from '../componentes/GameCollectionApp';
 import { VideojuegosApp } from '../VideojuegosApp';
+import { WelcomePage } from '../componentes/WelcomePage';
 
 
 export const SecondaryRouter = () => {
@@ -10,7 +11,6 @@ export const SecondaryRouter = () => {
     <><Navbar />
         <div>
             <Switch>
-
             <Route exact path="/coleccion" component={GameCollectionApp} />
             <Route exact path="/videojuegos" component={VideojuegosApp} />                     
                 
