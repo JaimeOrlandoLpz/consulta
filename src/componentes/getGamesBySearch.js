a9e82fbc767ce11906a6f219d5e7c29bbb307a07
Jaime Orlando Lopez Ramos <30807749+JaimeOrlandoLpz@users.noreply.github.com>
2021-11-07 12:26:30 -0600

    búsqueda id

--- src/componentes/getGamesBySearch.js
+++ src/componentes/getGamesBySearch.js
@@ -1,17 +1,33 @@
+import { getVideojuegosByID } from "./GetVideoJuegosByID";
+
 export const getGamesBySearch = (busqueda = '') => {
     if (busqueda == ''){
         return [];
     }
+    let busquedaInt = parseInt(busqueda, 10);
     if(localStorage.videogames){
         console.log('games = ' + localStorage.videogames);
         var query = [];
         var juegos = JSON.parse(localStorage.videogames.toString())
         for (var juegoIndice in juegos) {
             var juego = juegos[juegoIndice];
-            var name = juego["videogames"].name;
-            if (name.toLowerCase().includes(busqueda.toLowerCase())) {
-                query.push(juego);
+
+            // índice del juego en local storage
+            if(juego=== busquedaInt){
+                console.log("Yaaay");
+                let jueguito;
+                getVideojuegosByID(juego).then((res)=>{
+                    jueguito = res;
+                    console.log(res);
+                });
+                
+
             }
+
+            else{
+                console.log("Not Found Yet");
+            }
+
         }
         return query;
     }
